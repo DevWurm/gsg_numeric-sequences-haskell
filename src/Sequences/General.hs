282aed13598103c0047a912988da0dea1a3c517a
DevWurm <devwurm@gmx.net>
2016-02-11 22:49:38 +0100

    Fixing definition of recursive sequences. Old definition was depending on old interface which was changed in bb2f03.

--- src/Sequences/General.hs
+++ src/Sequences/General.hs
@@ -7,7 +7,7 @@ module Sequences.General (
 type Sequence a = [a]
 
 newRecSequence :: (Real a) =>  a -> (a -> a) -> Sequence a
-newRecSequence a0 f = a0 : map f (newRecSequence f a0)
+newRecSequence a0 f = a0 : map f (newRecSequence a0 f)
 
 newExpSequence :: (Real a) => (Integer -> a) -> Sequence a
 newExpSequence f = map f [1 .. ]
