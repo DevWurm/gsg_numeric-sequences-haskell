9d9f5bc9bec8b1a0e0e4d9a62f752ca2d16801ad
DevWurm <devwurm@gmx.net>
2016-02-11 22:39:36 +0100

    Adding Arithmetic Sequences Definitions

---
+++ src/Sequences/Arithmetic.hs
@@ -0,0 +1,12 @@
+module Sequences.Arithmetic (
+    newRecArSequence,
+    newExpArSequence
+) where
+
+import Sequences.General
+
+newRecArSequence :: (Real a) => a -> a -> Sequence a
+newRecArSequence a0 p = newRecSequence a0 (+p)
+
+newExpArSequence :: (Real a) => (Integer -> a) -> Sequence a
+newExpArSequence a0 p = newExpSequence (\n -> a0 + n * p)
