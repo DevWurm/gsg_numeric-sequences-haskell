bb2f03c9518cadbfeede5cbee3417484ef15bd03
DevWurm <devwurm@gmx.net>
2016-02-11 22:35:02 +0100

    Changing interface for newRecSequence function to a more convenient order

--- src/Sequences/General.hs
+++ src/Sequences/General.hs
@@ -2,8 +2,8 @@ module Sequences.General where
 
 type Sequence a = [a]
 
-newRecSequence :: (Real a) => (a -> a) -> a -> Sequence a
-newRecSequence f a0 = a0 : map f (newRecSequence f a0)
+newRecSequence :: (Real a) =>  a -> (a -> a) -> Sequence a
+newRecSequence a0 f = a0 : map f (newRecSequence f a0)
 
 newExpSequence :: (Real a) => (Integer -> a) -> Sequence a
 newExpSequence f = map f [1 .. ]
