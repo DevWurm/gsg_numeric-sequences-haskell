cfaf88d64dcff2b2ab90911b6cf2c713bfdaaa01
DevWurm <devwurm@gmx.net>
2016-02-12 18:59:31 +0100

    Fixing incorrect typing in Sequences.Arithmetic module

--- src/Sequences/Arithmetric.hs
+++ src/Sequences/Arithmetric.hs
@@ -9,4 +9,4 @@ newRecArSequence :: (Real a) => a -> a -> Sequence a
 newRecArSequence a0 p = newRecSequence a0 (+p)
 
 newExpArSequence :: (Real a) => a -> a -> Sequence a
-newExpArSequence a0 p = newExpSequence (\n -> a0 + n * p)
+newExpArSequence a0 p = newExpSequence (\n -> a0 + (fromInteger n) * p)
