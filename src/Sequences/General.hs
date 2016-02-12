e8abdf54e7aae1ad2d193ce62acfdd1391e3ba13
DevWurm <devwurm@gmx.net>
2016-02-12 22:36:49 +0100

    Adding the partialSumSequence function to the Sequences.General module. This functions generates te partial sum sequence of a provided sequence.

--- src/Sequences/General.hs
+++ src/Sequences/General.hs
@@ -14,3 +14,14 @@ newExpSequence f = map f [1 .. ]
 
 sumUntil :: (Real a) => Int -> Sequence a -> a
 sumUntil n = sum . take n
+
+partialSumSequence :: (Real a) => Sequence a -> Sequence a
+partialSumSequence seq = let
+                              {- Creating an infinite list of functions which sum the first n elements of a list.
+                                 n is beginning by 1. -}
+                              sumFunctions = map (\n -> sum . take n) [1 .. ]
+                         in
+                              {- Creating the infinite list of results of the sumFunctions functions applied to the
+                                 provided sequence. This list is equivalent to the partial sum sequence of the provided
+                                 sequence. -}
+                              map ($ seq) sumFunctions
