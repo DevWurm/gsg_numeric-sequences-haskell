d84269664c07b428d8c9310b10cb0d7e5525bccf
DevWurm <devwurm@gmx.net>
2016-02-13 10:26:55 +0100

    Adding module Sequences.Special. This module defines special (not named explicitely) sequences described in the project specifications.

---
+++ src/Sequences/Special.hs
@@ -0,0 +1,29 @@
+module Sequences.Special where
+
+import Sequences.General
+
+gs1 :: Sequence Double
+gs1 = newRecSequence 1 (\n0 -> 1 / (1 + n0))
+
+gs2 :: Sequence Double
+gs2 = newRecSequence (1 / (sqrt 2)) (\n0 -> 1 / (sqrt (2 + n0)))
+
+z :: Sequence Double
+z = newExpSequence (\n -> 1 / (fromInteger . fac $ n))
+
+fac :: Integer -> Integer
+fac 1 = 1
+fac n | n > 0 = n * (fac (n - 1))
+      | otherwise = error "fac is only defined for Integers bigger then 0"
+
+f :: Sequence Double
+f = newExpSequence (\n -> let
+                            intN = fromInteger n
+                          in
+                            (dFibs !! intN) / (dFibs !! (intN + 1)))
+    where
+        dFibs :: Sequence Double
+        dFibs = map fromInteger fibs
+
+fibs :: Sequence Integer
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
