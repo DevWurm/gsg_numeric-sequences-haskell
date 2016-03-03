e085e80cc88ca5cf7a49e62b03369d497a5c60a6
DevWurm <devwurm@gmx.net>
2016-03-03 19:44:26 +0100

    Adding the Inetrface.Top module, which provides an interface as entry into the interface.
    Adding the topMenue function, wich provides a main menue as entry into the interface.

---
+++ src/Interface/Top.hs
@@ -0,0 +1,28 @@
+module Interface.Top (
+    topMenue
+) where
+
+import Interface.General
+import Interface.ArGeo
+import Interface.Special
+import Control.Monad
+
+topMenue :: IO ()
+topMenue = do
+               putStrLn "<1> Operationen mit arithmetischen und geometrischen Zahlenfolgen"
+               putStrLn "<2> Operationen mit speziellen Zahlenfolgen"
+               putStrLn "<3> Beenden"
+               putStr "Menüpunkt: "
+               option <- getLine
+               putChar '\n'
+               state <- handleOption option
+               when (state == Redo) topMenue
+
+handleOption :: String -> IO MenueState
+handleOption ('1':_) = do
+                         arGeoMenue
+                         return Redo
+handleOption ('2':_) = do
+                         specialMenue
+                         return Redo
+handleOption _ = return Done
